dce401e9aa89b77ba751832b19475c7794653105
Maks Thimbarov <thimbarov@gmail.com>
2024-01-19 00:13:07 +0200

    redact file parser

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -18,17 +18,18 @@ class FileReader {
         })
             .split("\n")
             .map((row) => row.split(","))
-            .map((row) => {
-            return [
-                (0, utils_1.stringToDate)(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6],
-            ];
-        });
+            .map(this.mapRow);
+    }
+    mapRow(row) {
+        return [
+            (0, utils_1.stringToDate)(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ];
     }
 }
 exports.FileReader = FileReader;
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -15,16 +15,18 @@ export class FileReader {
    })
    .split("\n")
    .map((row: string): string[] => row.split(","))
-   .map((row: string[]): MatchData => {
-    return [
-     stringToDate(row[0]),
-     row[1],
-     row[2],
-     parseInt(row[3]),
-     parseInt(row[4]),
-     row[5] as MatchResult,
-     row[6],
-    ];
-   });
+   .map(this.mapRow);
+ }
+
+ mapRow(row: string[]): MatchData {
+  return [
+   stringToDate(row[0]),
+   row[1],
+   row[2],
+   parseInt(row[3]),
+   parseInt(row[4]),
+   row[5] as MatchResult,
+   row[6],
+  ];
  }
 }
